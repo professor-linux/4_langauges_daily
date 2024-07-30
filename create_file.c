892f76ec4a98b01878651f79ddc359bd1601acf1
Professor Linux <123519116+professor-linux@users.noreply.github.com>
2024-07-30 00:32:20 -0400

    Create create_file.c

---
+++ create_file.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+// Need to add a \n after the text
+
+
+int main(int argc, char *argv[]){
+
+	if (argc < 3) {
+	   fprintf(stderr, "Usage: %s <filename> <text>\n", argv[0]);
+	   return 1;
+	}
+
+	char *filename = argv[1];
+	char *text = argv[2];
+        size_t length = strlen(text);
+
+	if( access(filename, F_OK) == 0){
+		printf("%s exists, appending to file.\n",argv[1]);
+	        int result = open(filename, O_WRONLY | O_APPEND, 0644);
+	        size_t bw = write(result, text, length);
+	        printf("Bytes Written %ld\n", bw);
+		close(result);	
+	}
+	else {
+	     int result = open(filename, O_CREAT | O_RDWR);
+             size_t bw = write(result, text, length);
+             printf("Bytes Written %ld\n", bw);
+	     close(result);
+
+	}
+
+	return 0;
+}
